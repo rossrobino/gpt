d3f26ad170126b85548bfd55a3f072066944d11a
rossrobino <rbrobino@gmail.com>
2023-03-28 11:49:59 -0700

    remove edge

--- svelte.config.js
+++ svelte.config.js
@@ -11,6 +11,6 @@ export default {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({ runtime: "edge" }),
+		adapter: adapter(),
 	},
 };
