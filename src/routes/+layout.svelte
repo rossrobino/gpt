a526f70d4d7faaed7f62203a1d31c9fa3c062db5
rossrobino <rbrobino@gmail.com>
2023-05-02 14:34:53 -0700

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+	import { dev } from "$app/environment";
 	import "../app.postcss";
+	import { inject } from "@vercel/analytics";
+
+	inject({ mode: dev ? "development" : "production" });
 </script>
 
 <main
