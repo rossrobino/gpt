f52643ef324b3eb63e6f01f61ff6c072835a7ee4
rossrobino <rbrobino@gmail.com>
2023-08-24 21:48:10 -0400

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
-	 to read the documentation
-</p>
+<script lang="ts">
+	import type { ChatCompletionMessage } from "openai/resources/chat";
+	import { Editor } from "drab";
+
+	let messages: ChatCompletionMessage[] = [];
+
+	let content = "";
+	let completion: ChatCompletionMessage;
+
+	let text = "";
+
+	const chat = async (e: SubmitEvent) => {
+		messages.push({ role: "user", content });
+		const response = await fetch("/api/chat", {
+			method: "POST",
+			body: JSON.stringify(messages),
+			headers: {
+				"content-type": "application/json",
+			},
+		});
+		if (response.body) {
+			const reader = response.body.getReader();
+			let chunk = await reader.read();
+			while (!chunk.done) {
+				const value = new TextDecoder("utf-8").decode(chunk.value);
+				text += value;
+				chunk = await reader.read();
+			}
+		}
+	};
+</script>
+
+{text}
+
+{JSON.stringify(messages)}
+
+<form on:submit|preventDefault={chat}>
+	<Editor classTextarea="input" bind:valueTextarea={content} />
+	<button>Submit</button>
+</form>
---
+++ src/routes/api/chat/+server.ts
@@ -0,0 +1,41 @@
+import { OpenAI } from "openai";
+import type { ChatCompletionMessage } from "openai/resources/chat";
+import { OPENAI_API_KEY } from "$env/static/private";
+import { error } from "@sveltejs/kit";
+
+export const POST = async ({ request }) => {
+	const messages = (await request.json()) as ChatCompletionMessage[];
+
+	console.log(messages);
+
+	const openai = new OpenAI({
+		apiKey: OPENAI_API_KEY,
+	});
+
+	try {
+		const stream = await openai.chat.completions.create({
+			messages,
+			model: "gpt-3.5-turbo",
+			stream: true,
+		});
+
+		const readable = new ReadableStream({
+			start: async (controller) => {
+				for await (const part of stream) {
+					// instead of writing out here
+					controller.enqueue(part.choices[0]?.delta?.content || "");
+				}
+			},
+		});
+
+		return new Response(readable, {
+			headers: { "content-type": "text/plain" },
+		});
+	} catch (e) {
+		if (e instanceof OpenAI.APIError && e.status) {
+			console.error(e);
+			throw error(e.status, e.name);
+		}
+		throw error(500, "something went wrong.");
+	}
+};
