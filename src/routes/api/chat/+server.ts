f6c980645964e34a4ce40d2c0593c60973abcec2
rossrobino <rbrobino@gmail.com>
2023-09-07 09:33:54 -0400

    model

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -13,8 +13,8 @@ export const POST = async ({ request }) => {
 	try {
 		const stream = await openai.chat.completions.create({
 			messages,
-			model: "gpt-3.5-turbo",
-			// model: "gpt-4",
+			// model: "gpt-3.5-turbo",
+			model: "gpt-4",
 			stream: true,
 		});
 
