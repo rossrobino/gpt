2b826d0f498984bc10987c57d66e5cdfb63d9132
rossrobino <rbrobino@gmail.com>
2025-04-04 07:55:03 -0400

    loading

--- src/server/+app.tsx
+++ src/server/+app.tsx
@@ -3,7 +3,7 @@ import { Home } from "@/server/home";
 import { Messages, Message, type MessageEntry } from "@/ui/messages";
 import { html } from "client:page";
 import type { ResponseInput } from "openai/resources/responses/responses.mjs";
-import { escape, Router } from "ovr";
+import { escape, Router, Suspense } from "ovr";
 
 const app = new Router({
 	start(c) {
@@ -59,83 +59,95 @@ app.post("/", async (c) => {
 
 	c.page(
 		<Home web={web} model={model}>
-			{/* required for safari streaming */}
-			<div style="width: 0; height: 0">{"\u200b".repeat(200)}</div>
-
 			<Messages messages={messages} />
 
-			{async function* () {
-				yield '<div class="py-6 chat-bubble">';
-
-				const oModel = model.startsWith("o");
-
-				const input: ResponseInput = [
-					...messages.map((v) => v.message).slice(0, -1),
-					{ role: "user", content: newMessage },
-				];
-
-				if (oModel) {
-					// needed for o3-mini for some reason
-					input.unshift({
-						role: "user",
-						content:
-							"Use markdown syntax for your responses. For example, surround codeblocks in three backticks with the language if you are writing code. I don't want you to put the entire message in a markdown codeblock just use the syntax to respond.",
-					});
+			<Suspense
+				fallback={
+					<p class="my-5">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							class="size-5 animate-pulse"
+						>
+							<path d="M15.98 1.804a1 1 0 0 0-1.96 0l-.24 1.192a1 1 0 0 1-.784.785l-1.192.238a1 1 0 0 0 0 1.962l1.192.238a1 1 0 0 1 .785.785l.238 1.192a1 1 0 0 0 1.962 0l.238-1.192a1 1 0 0 1 .785-.785l1.192-.238a1 1 0 0 0 0-1.962l-1.192-.238a1 1 0 0 1-.785-.785l-.238-1.192ZM6.949 5.684a1 1 0 0 0-1.898 0l-.683 2.051a1 1 0 0 1-.633.633l-2.051.683a1 1 0 0 0 0 1.898l2.051.684a1 1 0 0 1 .633.632l.683 2.051a1 1 0 0 0 1.898 0l.683-2.051a1 1 0 0 1 .633-.633l2.051-.683a1 1 0 0 0 0-1.898l-2.051-.683a1 1 0 0 1-.633-.633L6.95 5.684ZM13.949 13.684a1 1 0 0 0-1.898 0l-.184.551a1 1 0 0 1-.632.633l-.551.183a1 1 0 0 0 0 1.898l.551.183a1 1 0 0 1 .633.633l.183.551a1 1 0 0 0 1.898 0l.184-.551a1 1 0 0 1 .632-.633l.551-.183a1 1 0 0 0 0-1.898l-.551-.184a1 1 0 0 1-.633-.632l-.183-.551Z" />
+						</svg>
+					</p>
 				}
+			>
+				{async function* () {
+					const oModel = model.startsWith("o");
 
-				const response = await ai.openai.responses.create({
-					model,
-					reasoning: oModel ? { effort: "high" } : undefined,
-					input,
-					stream: true,
-					tools: web && !oModel ? [{ type: "web_search_preview" }] : [],
-				});
+					const input: ResponseInput = [
+						...messages.map((v) => v.message).slice(0, -1),
+						{ role: "user", content: newMessage },
+					];
 
-				const htmlStream = processor.renderStream(
-					new ReadableStream<string>({
-						async start(c) {
-							for await (const event of response) {
-								if (event.type === "response.output_text.delta")
-									if (event.delta) c.enqueue(event.delta);
-							}
-
-							c.close();
-						},
-					}),
-				);
-
-				let finalContent = "";
-
-				const reader = htmlStream.getReader();
-
-				while (true) {
-					const { value, done } = await reader.read();
-					if (value) {
-						yield value;
-						finalContent += value;
+					if (oModel) {
+						// needed for o3-mini for some reason
+						input.unshift({
+							role: "user",
+							content:
+								"Use markdown syntax for your responses. For example, surround codeblocks in three backticks with the language if you are writing code. I don't want you to put the entire message in a markdown codeblock just use the syntax to respond.",
+						});
 					}
-					if (done) break;
-				}
 
-				yield "</div>";
+					const response = await ai.openai.responses.create({
+						model,
+						reasoning: oModel ? { effort: "high" } : undefined,
+						input,
+						stream: true,
+						tools: web && !oModel ? [{ type: "web_search_preview" }] : [],
+					});
 
-				yield (
-					<input
-						hidden
-						name={`content-${messages.length}`}
-						value={escape(finalContent, true)}
-					></input>
-				);
+					const htmlStream = processor.renderStream(
+						new ReadableStream<string>({
+							async start(c) {
+								for await (const event of response) {
+									if (event.type === "response.output_text.delta")
+										if (event.delta) c.enqueue(event.delta);
+								}
 
-				yield (
-					<Message
-						entry={{
-							index: messages.length + 1,
-							message: { role: "user", content: "" },
-						}}
-					/>
-				);
-			}}
+								c.close();
+							},
+						}),
+					);
+
+					let finalContent = "";
+
+					const reader = htmlStream.getReader();
+
+					for (let i = 0; ; i++) {
+						const { value, done } = await reader.read();
+						if (i === 0) yield '<div class="py-6 chat-bubble">'; // don't send early for loader
+
+						if (value) {
+							yield value;
+							finalContent += value;
+						}
+						if (done) break;
+					}
+
+					yield "</div>";
+
+					yield (
+						<input
+							hidden
+							name={`content-${messages.length}`}
+							value={escape(finalContent, true)}
+						></input>
+					);
+
+					yield (
+						<Message
+							entry={{
+								index: messages.length + 1,
+								message: { role: "user", content: "" },
+							}}
+						/>
+					);
+				}}
+			</Suspense>
 		</Home>,
 	);
 });
