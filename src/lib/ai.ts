ad93d1a23c42ee055c896ea52445638e3c00aaf8
rossrobino <rbrobino@gmail.com>
2025-04-14 13:54:58 -0400

    4.1

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -4,6 +4,5 @@ export const openai = new OpenAI({
 	apiKey: import.meta.env.VITE_OPENAI_API_KEY,
 });
 
-export const models = ["gpt-4o", "o3-mini"];
-
-export const defaultModel = "gpt-4o";
+export const models = ["gpt-4.1", "o3-mini"];
+export const defaultModel = models[0]!;
