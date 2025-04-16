fcbfb3c113f9781c97e076fdebc303f0272bb002
rossrobino <rbrobino@gmail.com>
2025-04-16 14:43:05 -0400

    group

--- src/ui/details.tsx
+++ src/ui/details.tsx
@@ -6,7 +6,7 @@ export const Details = (props: { children: JSX.Element; summary: string }) => (
 			<div class="text-muted-foreground opacity-0 transition-opacity group-hover:opacity-100">
 				{props.summary}
 			</div>
-			<div class="hover:bg-muted rounded-sm p-2 transition hover:shadow active:scale-97 active:shadow-none dark:shadow-black/75">
+			<div class="group-hover:bg-muted rounded-sm p-2 transition group-hover:shadow group-active:scale-97 group-active:shadow-none dark:shadow-black/75">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 20 20"
