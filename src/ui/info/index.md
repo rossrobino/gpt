34da7c9b52539e4a85833eeaf8024cce015eeffc
rossrobino <rbrobino@gmail.com>
2025-04-17 15:37:43 -0400

    info

--- src/ui/info/index.md
+++ src/ui/info/index.md
@@ -1,3 +1,6 @@
-- Select the globe button to toggle web search capabilities (only supported on select models).
-- Start a message with a URL (`https://...`) to include the content of the page in your message.
-  - GitHub links will attempt to pull the source code for the file referenced.
+- **Models**
+  - The latest [OpenAI](https://platform.openai.com/docs/models) models are available to use.
+- **Web Search**
+  - Select the globe button to toggle web search capabilities (only supported on select models).
+- **Web Content**
+  - Start a message with a URL (https://) to include the content of the page in your message.
--- src/ui/info/index.tsx
+++ src/ui/info/index.tsx
@@ -9,10 +9,10 @@ export const Info = () => {
 			<div
 				id="info"
 				popover
-				class="bg-muted border-base-200 dark:border-base-800 backdrop:bg-background/60 m-auto mx-5 my-auto rounded-md border p-6 opacity-0 shadow-2xl transition-[display,opacity] transition-discrete duration-300 backdrop:opacity-0 backdrop:backdrop-blur-lg backdrop:transition-[display,opacity] backdrop:transition-discrete backdrop:duration-300 open:opacity-100 open:backdrop:opacity-100 motion-reduce:duration-0 sm:mx-auto sm:max-w-[75ch] starting:open:opacity-0 starting:open:backdrop:opacity-0"
+				class="bg-background border-base-200 dark:border-base-800 backdrop:bg-muted/60 m-auto mx-5 my-auto rounded-md border p-6 opacity-0 shadow-2xl transition-[display,opacity] transition-discrete duration-300 backdrop:opacity-0 backdrop:backdrop-blur-lg backdrop:transition-[display,opacity] backdrop:transition-discrete backdrop:duration-300 open:opacity-100 open:backdrop:opacity-100 motion-reduce:duration-0 sm:mx-auto sm:max-w-[75ch] starting:open:opacity-0 starting:open:backdrop:opacity-0"
 			>
 				<div class="mb-4 flex items-center justify-between">
-					<div class="text-lg font-bold">Welcome</div>
+					<div class="text-lg font-bold">Features</div>
 					<Trigger hide />
 				</div>
 				{html}
