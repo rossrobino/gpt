440b9f3fd8f809ad8a84333d9ff45e54f5a051a8
rossrobino <rbrobino@gmail.com>
2025-05-27 14:17:54 -0400

    clean up

--- src/server/home.tsx
+++ src/server/home.tsx
@@ -28,13 +28,14 @@ export const action = new Action("/chat", async (c) => {
 	const data = await c.req.formData();
 
 	const id = schema.NullableStringSchema.parse(data.get("id"));
-	const newId = new Deferred<string>();
-
-	let text = schema.StringSchema.parse(data.get("text"));
 	const web = data.get("web") === "on";
 	const model =
 		ai.models.find((m) => m.name === data.get("model")) ?? ai.defaultModel;
-	const messageIndex = schema.StringSchema.parse(data.get("index"));
+	let text = schema.StringSchema.parse(data.get("text"));
+	let messageIndex = parseInt(schema.StringSchema.parse(data.get("index")));
+
+	const newId = new Deferred<string>();
+	const finalMessageIndex = new Deferred<number>();
 
 	c.head(<title>{generateTitle(data)}</title>);
 
@@ -88,88 +89,82 @@ export const action = new Action("/chat", async (c) => {
 				}
 
 				// yield current messages
-				let i = parseInt(messageIndex);
-				for (const message of content) {
-					yield (
-						<Message
-							transitionName={`m-${i}`}
-							message={{ role: "user", content: [message] }}
-						/>
-					);
+				yield content.map((message) => (
+					<Message
+						transitionName={`m-${messageIndex++}`}
+						message={{ role: "user", content: [message] }}
+					/>
+				));
 
-					i++;
-				}
+				finalMessageIndex.resolve(messageIndex);
 
 				yield (
-					<>
-						<div class="chat-bubble py-6">
-							{async function* () {
-								const htmlStream = processor.renderStream(
-									new ReadableStream<string>({
-										async start(c) {
-											const response = await ai.openai.responses.create({
-												input: [{ role: "user", content }],
-												instructions,
-												model: model.name,
-												reasoning: model.reasoning
-													? { effort: "medium" }
-													: undefined,
-												tools:
-													web && model.web
-														? [{ type: "web_search_preview" }]
-														: [],
-												stream: true,
-												truncation: "auto",
-												store: true,
-												previous_response_id: id,
-											});
+					<div class="chat-bubble py-6">
+						{async function* () {
+							const htmlStream = processor.renderStream(
+								new ReadableStream<string>({
+									async start(c) {
+										const response = await ai.openai.responses.create({
+											input: [{ role: "user", content }],
+											instructions,
+											model: model.name,
+											reasoning: model.reasoning
+												? { effort: "medium" }
+												: undefined,
+											tools:
+												web && model.web
+													? [{ type: "web_search_preview" }]
+													: [],
+											stream: true,
+											truncation: "auto",
+											store: true,
+											previous_response_id: id,
+										});
 
-											for await (const event of response) {
-												if (
-													event.type === "response.output_item.added" &&
-													event.item.type === "reasoning"
-												) {
-													c.enqueue("Reasoning...\n\n");
-												} else if (
-													event.type === "response.output_text.delta"
-												) {
-													if (event.delta) c.enqueue(event.delta);
-												} else if (event.type === "response.completed") {
-													newId.resolve(event.response.id);
-												}
+										for await (const event of response) {
+											if (
+												event.type === "response.output_item.added" &&
+												event.item.type === "reasoning"
+											) {
+												c.enqueue("Reasoning...\n\n");
+											} else if (event.type === "response.output_text.delta") {
+												if (event.delta) c.enqueue(event.delta);
+											} else if (event.type === "response.completed") {
+												newId.resolve(event.response.id);
 											}
+										}
 
-											c.close();
-										},
-									}),
-								);
+										c.close();
+									},
+								}),
+							);
 
-								const reader = htmlStream.getReader();
-								while (true) {
-									const { value, done } = await reader.read();
-									if (value) yield value;
-									if (done) break;
-								}
-							}}
-						</div>
-
-						<Input index={parseInt(messageIndex) + 1} />
-						<Controls model={model} web={web} />
-
-						{async () => (
-							<input
-								type="hidden"
-								name="title"
-								value={await generateTitle(data)}
-							></input>
-						)}
-
-						{async () => (
-							<input type="hidden" name="id" value={await newId.promise} />
-						)}
-					</>
+							const reader = htmlStream.getReader();
+							while (true) {
+								const { value, done } = await reader.read();
+								if (value) yield value;
+								if (done) break;
+							}
+						}}
+					</div>
 				);
 			}}
+
+			{async () => <Input index={await finalMessageIndex.promise} />}
+
+			<Controls model={model} web={web} />
+
+			{async () => (
+				<input
+					type="hidden"
+					name="title"
+					value={await generateTitle(data)}
+				></input>
+			)}
+
+			{async () => (
+				<input type="hidden" name="id" value={await newId.promise} />
+			)}
 		</>
 	);
 });
