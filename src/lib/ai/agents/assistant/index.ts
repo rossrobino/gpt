e435a7d09f4d7b0b2518206924e6850b30825817
rossrobino <rbrobino@gmail.com>
2025-06-08 14:49:04 -0400

    display agents

--- src/lib/ai/agents/assistant/index.ts
+++ src/lib/ai/agents/assistant/index.ts
@@ -4,6 +4,6 @@ export const agent = new Agent({
 	name: "Assistant",
 	model: "gpt-4.1-mini",
 	handoffDescription:
-		"This agent should be the default, it is a generalist who has access to the web.",
+		"This agent is the default, a generalist who has access to the web.",
 	tools: [webSearchTool()],
 });
