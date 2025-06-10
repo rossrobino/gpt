c5c1ce2053ea8da65971e2018594684d23ce44c1
rossrobino <rbrobino@gmail.com>
2025-06-10 10:17:44 -0400

    semi

--- src/ui/popover.tsx
+++ src/ui/popover.tsx
@@ -25,7 +25,7 @@ export const Popover = (props: {
 				class="bg-background border-base-200 dark:border-base-800 backdrop:bg-muted/60 mx-5 my-auto max-h-[90dvh] overflow-y-auto rounded-md border p-6 opacity-0 shadow-2xl transition-[display,opacity] transition-discrete duration-300 backdrop:opacity-0 backdrop:backdrop-blur-lg backdrop:transition-[display,opacity] backdrop:transition-discrete backdrop:duration-300 open:opacity-100 open:backdrop:opacity-100 motion-reduce:duration-0 sm:mx-auto sm:max-w-[60ch] starting:open:opacity-0 starting:open:backdrop:opacity-0"
 			>
 				<div class="flex items-center justify-between">
-					<div class="cursor-default text-xl font-bold">{title}</div>
+					<div class="cursor-default text-xl font-semibold">{title}</div>
 					<Trigger id={id}>{<svg.X />}</Trigger>
 				</div>
 				<div class="pt-6">{children}</div>
