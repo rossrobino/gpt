3b52d9a55b74ba9b858b0d5a610cc7729204a789
rossrobino <rbrobino@gmail.com>
2025-06-11 13:32:35 -0400

    ctrl/meta + enter submit

--- src/client/main.ts
+++ src/client/main.ts
@@ -3,3 +3,12 @@ import "@fontsource-variable/geist-mono";
 import "drab/editor/define";
 import "drab/prefetch/define";
 import "drab/share/define";
+
+const form = document.querySelector("form");
+if (!form) throw Error("Form element not found.");
+
+form.addEventListener("keydown", (e) => {
+	if ((e.metaKey || e.ctrlKey) && e.key === "Enter") {
+		form.submit();
+	}
+});
